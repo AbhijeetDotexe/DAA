f1b2ab6f6d1e8a639da7f3cfae83062f28564412
TheMainManIND <82932131+TheMainManIND@users.noreply.github.com>
2021-05-12 10:43:37 +0530

    Add files via upload

---
+++ RandomizedQuickSort.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+inline void swap(int& a,int& b) {
+    int c = a;
+    a = b;
+    b = c;
+}
+
+int partition(int* array, int start, int end) {
+    int pivotElement = array[end];
+    int i = start-1;
+    for(int j=start; j<end; j++) {
+        if(array[j] <= pivotElement) {
+            i++;
+            swap(array[i], array[j]);
+        }
+    }
+    swap(array[i+1],array[end]);
+    return i+1;
+}
+
+int randomizedPartition(int* array, int start, int end) {
+    int randomPivotIndex = (rand()%(end-start)) + start;
+    swap(array[end], array[randomPivotIndex]);
+    return partition(array, start, end);
+}
+
+
+void randomizedQuickSort(int* array, int start, int end) {
+    if(start<end) {
+        int pivotIndex = randomizedPartition(array, start, end);
+        randomizedQuickSort(array, start, pivotIndex-1);
+        randomizedQuickSort(array, pivotIndex+1, end);
+    }
+}
+
+int main() {
+    srand(time(0));
+    int array[] = {9,8,7,6,5,4,3,2,1,0};
+    randomizedQuickSort(array, 0, 9);
+    for(int i=0; i<10; i++) {
+        cout<<array[i]<<" ";
+    }
+    cout<<endl;
+}
